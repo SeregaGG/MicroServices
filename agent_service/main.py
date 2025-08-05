c110dc9279dca49fbed43e7f11615a09a06a9cb7
Serega <serega.taraskin.1999@mail.ru>
2025-08-05 22:38:04 +0500

    fix v2

--- agent_service/main.py
+++ agent_service/main.py
@@ -1,13 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from app.api import agent
 
 from app.core.config import settings
 
+load_dotenv()
+service_name = os.getenv('SERVICE_NAME', 'test')
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    openapi_url=f"/{service_name}{settings.API_V1_STR}/openapi.json",
 )
 
 app.include_router(agent.router, prefix=settings.API_V1_STR)
