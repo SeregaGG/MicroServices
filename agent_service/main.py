481d13344208b90b22285b3f7f5361e2e4ec051e
Serega <serega.taraskin.1999@mail.ru>
2025-08-05 22:40:55 +0500

    test

--- agent_service/main.py
+++ agent_service/main.py
@@ -13,7 +13,7 @@ service_name = os.getenv('SERVICE_NAME', 'test')
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
-    openapi_url=f"/{service_name}{settings.API_V1_STR}/openapi.json",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
 app.include_router(agent.router, prefix=settings.API_V1_STR)
